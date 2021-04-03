64c05f454545549a45ee93ab0857cf77144bb891
MoonWolf63 <39937329+MoonWolf63@users.noreply.github.com>
2021-04-02 23:36:13 -0400

    Fixed Huge Problem
    Switched Balance, added bank, and changed bank and Balance into INT

--- sql/projectalpha.sql
+++ sql/projectalpha.sql
@@ -1,50 +1,49 @@
--- MySQL dump 10.13  Distrib 5.7.32, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: projectalpha
--- ------------------------------------------------------
--- Server version	5.7.32-0ubuntu0.18.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2021 at 05:35 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `economy`
 --
 
-DROP TABLE IF EXISTS `economy`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `economy` (
   `ID` text NOT NULL,
-  `Balance` text NOT NULL
+  `Balance` int(255) NOT NULL,
+  `Bank` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `economy`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `economy` WRITE;
-/*!40000 ALTER TABLE `economy` DISABLE KEYS */;
-INSERT INTO `economy` VALUES ('0','0');
-/*!40000 ALTER TABLE `economy` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `economy`
+--
+ALTER TABLE `economy`
+  ADD PRIMARY KEY (`Balance`);
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-12-04  6:11:26
